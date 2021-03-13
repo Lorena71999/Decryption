9024e6b5441718e357c3d3970e0c3d47f1b20bb7
Maria-Lorena Comanescu <61329796+Lorena71999@users.noreply.github.com>
2021-03-13 13:26:33 +0200

    Add files via upload

---
+++ decryption_regfile.v
@@ -0,0 +1,166 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    20:13:49 11/23/2020 
+// Design Name: 
+// Module Name:    decryption_regfile 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module decryption_regfile #(
+			parameter addr_witdth = 8,
+			parameter reg_width   = 16
+		)(
+			// Clock and reset interface
+			input clk, 
+			input rst_n,
+			
+			// Register access interface
+			input[addr_witdth - 1:0] addr,
+			input read,
+			input write,
+			input [reg_width -1 : 0] wdata,
+			output reg [reg_width -1 : 0] rdata,
+			output reg done,
+			output reg error,
+			
+			// Output wires
+			output reg[reg_width - 1 : 0] select,
+			output reg[reg_width - 1 : 0] caesar_key,
+			output reg[reg_width - 1 : 0] scytale_key,
+			output reg[reg_width - 1 : 0] zigzag_key
+    );
+           `define SELECT_REGISTER       8'h00
+	       `define CAESAR_KEY_REGISTER   8'h10
+	       `define SCYTALE_KEY_REGISTER  8'h12
+	       `define ZIGZAG_KEY_REGISTER   8'h14
+	 
+	 
+    always @(posedge clk) begin
+
+       if(rst_n == 0)begin
+	 
+	      select      <= 16'h0;
+		  caesar_key  <= 16'h0;
+		  scytale_key <= 16'hFFFF;
+		  zigzag_key  <= 16'h02;
+		 
+	   end  
+		 
+	   else if(write == 1) begin
+					 
+		    case(addr)
+		   
+			`SELECT_REGISTER:begin
+			
+				    select <= {14'b0,wdata[1:0]};
+				   
+					end
+					
+			`CAESAR_KEY_REGISTER:begin
+			 
+				   caesar_key <= wdata;
+				   
+				   end
+				   
+			`SCYTALE_KEY_REGISTER:begin
+			
+				  scytale_key <= wdata;
+				  
+				   end
+				   
+			`ZIGZAG_KEY_REGISTER:begin	
+			
+				  zigzag_key <= wdata;
+				  
+				  end
+		  
+		    endcase
+	 
+		    if((addr != `SELECT_REGISTER) && (addr != `CAESAR_KEY_REGISTER) && (addr != `SCYTALE_KEY_REGISTER) && (addr != `ZIGZAG_KEY_REGISTER)) begin
+			
+			  error <= 1;
+			  
+		    end
+			
+		    else begin
+		  
+			  error <= 0;
+			  
+		    end
+		  
+		  done <= 1;
+		  
+      end		 
+  
+	  else if(read == 1)begin
+			    
+			case(addr)
+			
+		     `SELECT_REGISTER:begin
+			 
+		             rdata <= select;
+					 
+			          end
+		     `CAESAR_KEY_REGISTER :begin
+					
+		              rdata <= caesar_key;
+						  
+		             end
+					 
+	 	     `SCYTALE_KEY_REGISTER:begin
+					
+		              rdata <= scytale_key;
+						  
+			          end
+					  
+		      `ZIGZAG_KEY_REGISTER:begin	
+					
+                      rdata <= zigzag_key;
+						  
+                      end
+			  
+            endcase
+				  
+		    if((addr != `SELECT_REGISTER) && (addr !=  `CAESAR_KEY_REGISTER) && (addr != `SCYTALE_KEY_REGISTER) && (addr != `ZIGZAG_KEY_REGISTER))begin
+			   
+		         error <= 1;
+				   
+		    end 
+				 
+		    else begin
+					
+			      error <= 0;
+					  
+			end
+				   
+				   done <= 1;
+	
+			 
+            end
+			
+            else begin
+			
+			     done  <= 0;
+			     error <= 0;
+			 
+			end 
+			
+	  
+    end
+
+
+	
+
+endmodule
---
+++ scytale_decryption.v
@@ -0,0 +1,142 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    19:24:12 11/27/2020 
+// Design Name: 
+// Module Name:    scytale_decryption 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module scytale_decryption#(
+			parameter D_WIDTH = 8, 
+			parameter KEY_WIDTH = 8, 
+			parameter MAX_NOF_CHARS = 50,
+			parameter START_DECRYPTION_TOKEN = 8'hFA
+		)(
+			// Clock and reset interface
+			input clk,
+			input rst_n,
+			
+			// Input interface
+			input[D_WIDTH - 1:0] data_i,
+			input valid_i,
+			
+			// Decryption Key
+			input[KEY_WIDTH - 1 : 0] key_N,
+			input[KEY_WIDTH - 1 : 0] key_M,
+			
+			// Output interface
+			output reg[D_WIDTH - 1:0] data_o,
+			output reg valid_o,
+			
+			output reg busy
+    );
+  
+	
+  
+	
+	
+	 reg[D_WIDTH - 1:0] vector [MAX_NOF_CHARS-1:0];
+	 
+	
+	 reg[7:0] state  ;
+	 reg[7:0] contor_1 ;
+	 reg[7:0]  j  ;
+	 reg[7:0]  i;
+	
+	  initial begin
+	 
+	      for(i = 0 ; i< MAX_NOF_CHARS; i = i + 1)
+	          vector[i] <= 0;
+		
+	  end
+	
+	 always@(posedge clk)begin
+	  
+	      if(rst_n == 0)begin
+		    data_o  <= 0; 
+		    valid_o <= 0; 
+		    busy    <= 0;
+		    state   <= 8'h0; 
+            j       <=0;
+            contor_1<=0;		 
+	     end  
+		
+	
+	 
+	
+	    case(state)
+	        8'h0:begin
+		         busy <= 0;
+			     if(valid_i == 1 && busy ==0  )begin
+		           if(data_i!= START_DECRYPTION_TOKEN)begin
+		              vector[contor_1]<= data_i;
+					  contor_1 <= contor_1 + 1; 						
+							
+	               end
+		           else if(data_i == START_DECRYPTION_TOKEN)begin
+					       busy<=1;
+		                   state <= 8'h1;
+								
+			       end  
+					
+				   end
+				   if(valid_i == 0)begin
+				      busy<=0;
+					  valid_o<=0;
+		      	      data_o<=0;
+				  end
+		    end
+			
+			  
+            8'h1:begin
+		        
+				 if(j < key_M*key_N && busy == 1)begin
+				    data_o <= vector[j];
+					valid_o<=1;
+				    j<=j+key_N;
+				 end
+				
+				 if(j+key_N>= key_M*key_N)begin
+			        j <= 1 + j +key_N - key_M*key_N;	 
+				 end 
+				
+				 else begin 
+				    j<= j + key_N;
+				 end
+				
+			     if(j == key_N*key_M-1)begin
+			        state<=8'h2;
+			     end 
+			  
+		    end	   
+			  
+	        8'h2:begin
+			
+			     busy    <= 0 ;
+		         valid_o <= 0;
+			     data_o  <=0;
+			     contor_1<=0;
+			     state   <= 8'h0;
+				 j       <=0; 
+			end
+  
+        endcase
+		 
+		 
+    end
+		  
+
+endmodule
+		
